d449e9db3eae454d96f68bafe9a6ad193f51afb1
Kaya  Ünal <kayaunal92@gmail.com>
2020-07-22 09:01:59 +0300

    +

---
+++ 2020-Yaz/Lab2/src/Hesaplayici.java
@@ -0,0 +1,20 @@
+
+public class Hesaplayici {
+
+	
+	private static double ortalamaHesaplayici;
+	
+	
+	public static double ortalamaBul(int yazili1, int yazili2) {
+		return ortalamaHesaplayici = (yazili1 + yazili2) / 2;
+	}
+
+	public static double ortalamaBul(int yazili1, int yazili2, int yazili3) {
+		return ortalamaHesaplayici = (yazili1 + yazili2 + yazili3) / 3;
+	}
+
+	public static double ortalamaBul(int yazili1, int yazili2, int yazili3, int yazili4) {
+		return ortalamaHesaplayici = (yazili1 + yazili2 + yazili3 + yazili4) / 4;
+	}
+
+}
